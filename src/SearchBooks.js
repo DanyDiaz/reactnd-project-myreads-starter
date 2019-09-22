1b73bda1cd42865474d939dd87f869f3eb81fb1f
Dany Díaz <dany_diaz_22@hotmail.com>
2019-09-21 22:38:20 -0600

    feat: Modify search functionality that works while typing without pressing enter
    New library used (lodash) to debounce the search functionality while the user is writing the query

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import { debounce } from 'lodash'
 
 class SearchBooks extends Component {
     state = {
         searchQuery: '',
         searched: false,
+        searching: false,
         latestFetchedBooks: []
     }
 
@@ -17,15 +19,28 @@ class SearchBooks extends Component {
         }));
     }
 
-    searchBooks(event) {
-        let {myBooks} = this.props;
+    searchBooks = debounce(() => {
+        this.setState(() => ({
+            latestFetchedBooks: [],
+            searching: true
+        }));
+        let initialQuery = this.state.searchQuery.trim();
         //it prevents to reload the page
-        event.preventDefault();
-        if(this.state.searchQuery.trim() !== '') {
+        if(initialQuery !== '') {
+            let booksApp = this;
+            let {myBooks} = this.props;
             //if the query is not empty, use the BooksAPI to search books with that
-            BooksAPI.search(this.state.searchQuery)
+            BooksAPI.search(initialQuery)
             .then((fetchedBooks) => {
-                if(fetchedBooks && fetchedBooks.length > 0) {
+                if(booksApp.state.searchQuery.trim() === '') {
+                    this.setState(() => ({
+                        latestFetchedBooks: [],
+                        searched: false,
+                        searching: false
+                    }));
+                }
+                else if(fetchedBooks && fetchedBooks.length > 0
+                        && initialQuery === booksApp.state.searchQuery) {
                     //Check each fetched book to see if they are already in the current shelves
                     //if that is the case, set the appropriate shelf
                     fetchedBooks.forEach(function(book) {
@@ -36,18 +51,28 @@ class SearchBooks extends Component {
                     });
 
                     this.setState(() => ({
-                        latestFetchedBooks: fetchedBooks
+                        latestFetchedBooks: fetchedBooks,
+                        searched: true,
+                        searching: false
                     }));
                 }
-                else {
+                else if(initialQuery === booksApp.state.searchQuery) {
                     this.setState(() => ({
                         latestFetchedBooks: [],
-                        searched: true
+                        searched: true,
+                        searching: false
                     }));
                 }
             });
         }
-    }
+        else {
+            this.setState(() => ({
+                latestFetchedBooks: [],
+                searched: false,
+                searching: false
+            }));
+        }
+    }, 600);
 
     render() {        
         let {shelvesOptions, changeShelfFunc} = this.props;
@@ -57,16 +82,12 @@ class SearchBooks extends Component {
             <div className="search-books-bar">
               <Link className="close-search" to='/'>Close</Link>
               <div className="search-books-input-wrapper">
-                <form
-                    onSubmit={(event) => this.searchBooks(event)}
-                >
-                    <input 
+                <input 
                     type="text"
                     value={this.state.searchQuery}
-                    placeholder="Search by title or author. Then press enter"
-                    onChange={(event) => this.changeSearchQuery(event.target.value)}
-                    />
-                </form>
+                    placeholder="Search by title or author."
+                    onChange={(event) => {this.changeSearchQuery(event.target.value); this.searchBooks()}}
+                />
               </div>
             </div>
             <div className="search-books-results">
@@ -83,8 +104,13 @@ class SearchBooks extends Component {
               </ol>
             </div>
             {(this.state.searched && this.state.latestFetchedBooks.length <= 0) && 
-                (<div className='no-books-message'>
+                (<div className='general-message'>
                     No books found with the term: <i>'{this.state.searchQuery}'</i> in it. Try with another query please.
+                </div>)
+            }
+            {(this.state.searching) && 
+                (<div className='general-message'>
+                    <i>Searching...</i>
                 </div>)
             }
           </div>
